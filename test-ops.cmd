d4fd776aef70bc2a9f478823f1ab2483b80d8f57
Benes <BenesOn1@vda.cz>
2025-06-06 11:32:49 +0200

    Pridani scitani

--- test-ops.cmd
+++ test-ops.cmd
@@ -2,3 +2,8 @@
 :start
 set /p prvni=Zadej prvni cislo: 
 set /p druhe=Zadej druhe cislo: 
+
+set /p vysledek=chces cisla secist, vynasobit, nebo vydelit? soucet/nysobeni/deleni
+if %vysledek%==%soucet% (
+set /a scitani=%prvni% + %druhe%
+echo %scitani%
